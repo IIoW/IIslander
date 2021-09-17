cc421cd68bbe4cf844df89c13101157af5a46b10
KommentatorForAll <46420447+KommentatorForAll@users.noreply.github.com>
2021-09-17 19:23:48 +0200

    fixed untested pushes

--- src/loadModules.js
+++ src/loadModules.js
@@ -2,26 +2,29 @@
 // import * as events from './modules/events';
 import fs from 'fs';
 
-const subscriptions = Map();
+const subscriptions = new Map();
 
-export default (client) => {
-
+export default async (client) => {
     /**
      * loads all modules and their subscriptions
      */
-    fs.readdirSync('./src/modules').forEach((moduleName) => {
+    const modules = fs.readdirSync('./src/modules');
+
+    for (const moduleName of modules) {
         // Loads the module
-        const module = import(`./src/modules/${moduleName}/module.js`);
+        // eslint-disable-next-line no-await-in-loop
+        const module = await import(`./modules/${moduleName}/module.js`);
         // skips the module, in case it is disabled.
-        if (!module.enabled) continue;
-        // Loads each of it's subscriptions into their according list.
-        module.subscriptions.forEach((event, fun) => {
-            if (!subscriptions.has(event)) {
-                subscriptions.set(event, []);
-            }
-            subscriptions.get(event).add(fun);
-        });
-    });
+        if (module.enabled) {
+            // Loads each of it's subscriptions into their according list.
+            module.subscriptions.forEach((fun, event) => {
+                if (!subscriptions.has(event)) {
+                    subscriptions.set(event, []);
+                }
+                subscriptions.get(event).push(fun);
+            });
+        }
+    }
 
     /**
      * Setting up all events.
@@ -29,18 +32,21 @@ export default (client) => {
 
     /**
      * Calles all provided functions with the given arguments.
+     * @param {import('discord.js').Client} client
+     * @param {Function[]} funs
+     * @param {*[]} args
      */
+    // eslint-disable-next-line no-shadow
     function callEventFunctions(client, funs, ...args) {
-        funs.forEach( fun => {
-            fun(client, ...args)
+        funs.forEach((fun) => {
+            fun(client, ...args);
         });
     }
 
     /**
      * binds all events inside the subscriptions map to call all functions provided
      */
-
-    subscriptions.forEach( (event, funs) => {
-        client.on(event, callEventFunctions.bind(null, client, funs))
+    subscriptions.forEach((funs, event) => {
+        client.on(event, callEventFunctions.bind(null, client, funs));
     });
-}
+};
--- src/modules/exampleModule/module.js
+++ src/modules/exampleModule/module.js
@@ -1,26 +1,24 @@
-
 function ready(client) {
-    // place your interenal functions here. 
-    // I'd recomment putting them in a different file and just passing the call through, to not messy this file.
-    console.log("Getting ready...");
-    console.log("---------------------");
+    // place your internal functions here.
+    // I'd recommend putting them in a different file and just passing the call through, to not messy this file.
+    console.log('Getting ready...');
+    console.log('---------------------');
     console.log(`Logged in as ${client.user.tag} - id: ${client.user.id}`);
-    console.log("---------------------");
+    console.log('---------------------');
 }
 
 function messageCreate(client, msg) {
-    // place your interenal functions here. 
-    // I'd recomment putting them in a different file and just passing the call through, to not messy this file.
-    console.log(msg.content)
+    // place your internal functions here.
+    // I'd recommend putting them in a different file and just passing the call through, to not messy this file.
+    console.log(msg.content);
 }
 
 /**
  * The events this module is subscribed to (entry points of this module, called when the specified event is triggered)
  */
-const subscriptions = {
-    'ready': ready,
-    'messageCreate': messageCreate
-}
+const subscriptions = new Map();
+subscriptions.set('ready', ready);
+subscriptions.set('messageCreate', messageCreate);
 
 /**
  * if this module should be loaded on startup
@@ -30,7 +28,4 @@ const enabled = true;
 /**
  * Opens up the subscription to be accessible from the outside.
  */
-export {
-    subscriptions,
-    enabled
-};
+export { subscriptions, enabled };
