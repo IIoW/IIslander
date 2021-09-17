8a6c44fc662f14e85658c1a6e9c19d4cc0743a25
Commentator2.0 <46420447+KommentatorForAll@users.noreply.github.com>
2021-09-17 10:27:34 +0200

    !!pushing untested code!! tried to implement event handling, with autoloading modules. This is not tested and may not even run. I only had some spare time at work and am unable to execute it here

--- src/loadModules.js
+++ src/loadModules.js
@@ -1,22 +1,67 @@
-import * as events from './modules/events';
+// Removing import temporary for now, until this is tested and works
+// import * as events from './modules/events';
+import fs from 'fs';
+
+const subscriptions = {
+    'ready': [],
+    'messageCreate': [],
+    'messageDelete': [],
+    'messageReactionAdd': [],
+    'messageReactionRemove': [],
+    'guildMemeberAdd': []
+};
 
 export default (client) => {
+
+    /**
+     * loads all modules and their subscriptions
+     */
+    fs.readdir('./src/modules').forEach((moduleName) => {
+        // Loads the module
+        const module = import(`./src/modules/${moduleName}/module.js`);
+        // skips the module, in case it is disabled.
+        if (!module.enabled) continue;
+        // Loads each of it's subscriptions into their according list.
+        module.subscriptions.forEach((event, fun) => {
+            subscriptions[event].add(fun);
+        });
+    });
+
+    /**
+     * Setting up all events.
+     * Currently all events have to be added manually,
+     * but if you know a way how to auto bind and pass the parameters, 
+     * feel free to change it up, that it binds all events specified in subscriptions
+     */
+
     client.on('ready', () => {
-        events.ready(client);
+        subscriptions['ready'].forEach(fun => {
+            fun(client);
+        });
     });
     client.on('messageCreate', (message) => {
-        events.messageCreate(client, message);
+        subscriptions['messageCreate'].forEach(fun => {
+            fun(client, message);
+        });
     });
     client.on('messageDelete', (message) => {
-        events.messageDelete(client, message);
+        subscriptions['messageDelete'].forEach(fun => {
+            fun(client, message);
+        });
     });
     client.on('messageReactionAdd', (reaction, user) => {
-        events.messageReactionAdd(client, reaction, user);
+        subscriptions['messageReactionAdd'].forEach(fun => {
+            fun(client, reaction, user);
+        });
     });
     client.on('messageReactionRemove', (reaction, user) => {
-        events.messageReactionRemove(client, reaction, user);
+        subscriptions['messageReactionRemove'].forEach(fun => {
+            fun(client, reaction, user);
+        });
     });
     client.on('guildMemberAdd', (member) => {
-        events.guildMemberAdd(client, member);
+        subscriptions['guildMemeberAdd'].forEach(fun => {
+            fun(client, member);
+        });
     });
 }
--- src/modules/exampleModule/module.js
+++ src/modules/exampleModule/module.js
@@ -1,5 +1,7 @@
 
 function ready(client) {
+    // place your interenal functions here. 
+    // I'd recomment putting them in a different file and just passing the call through, to not messy this file.
     console.log("Getting ready...");
     console.log("---------------------");
     console.log(`Logged in as ${client.user.tag} - id: ${client.user.id}`);
@@ -7,10 +9,28 @@ function ready(client) {
 }
 
 function messageCreate(client, msg) {
+    // place your interenal functions here. 
+    // I'd recomment putting them in a different file and just passing the call through, to not messy this file.
     console.log(msg.content)
 }
 
+/**
+ * The events this module is subscribed to (entry points of this module, called when the specified event is triggered)
+ */
+const subscriptions = {
+    'ready': ready,
+    'messageCreate': messageCreate
+}
+
+/**
+ * if this module should be loaded on startup
+ */
+const enabled = true;
+
+/**
+ * Opens up the subscription to be accessible from the outside.
+ */
 export {
-    ready,
-    messageCreate
+    subscriptions,
+    enabled
 };
