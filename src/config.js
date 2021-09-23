77fdb723a8fba351f2e5b77061960e28b8bf91eb
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2021-09-23 09:52:51 -0600

    chore: resolve review issues
    Add prefix to .env
    Subscribe directly to commands
    Properly set the module.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
     token: process.env.TOKEN,
     defaultGuild: process.env.DEFAULT_GUILD,
-    prefix: '//',
+    prefix: process.env.PREFIX,
     channels: new Map([
         ['notifications', process.env.CHANNEL_NOTIFICATION],
         ['starboard', process.env.CHANNEL_STARBOARD],
