bfd27de600538b822c3f3dc11fa06b2bce7b17f8
Antti <antti@antti.codes>
2021-10-03 17:17:21 +0300

    feat: welcome messages

---
+++ src/modules/welcome/module.js
@@ -0,0 +1,32 @@
+import { welcomeMessage, welcomeMessages, welcomeQuestions } from '../../constants/Messages';
+import { getChannel } from '../../util';
+
+const subscriptions = new Map();
+
+subscriptions.set('guildMemberAdd', async (client, member) => {
+    try {
+        await member.send(welcomeMessage);
+    } catch (e) {
+        /* ignore */
+    }
+
+    const welcomeChannel = getChannel('welcome');
+    const offtopicChannel = getChannel('just-chatting');
+
+    await welcomeChannel.send(
+        `${welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)].replaceAll(
+            '[name]',
+            member.user
+        )} Check out ${offtopicChannel} for a fun little question to answer!`
+    );
+
+    await offtopicChannel.send(
+        `I have a question for you ${member.user.username}! ${
+            welcomeQuestions[Math.floor(Math.random() * welcomeQuestions.length)]
+        }`
+    );
+});
+
+const enabled = true;
+
+export { subscriptions, enabled };
