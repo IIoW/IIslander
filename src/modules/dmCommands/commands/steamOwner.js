10ff1f2225203d1d191eac75fea3a440f2832cd9
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2021-10-12 14:45:07 -0600

    chore: add button handler
    Also made steamowner command use it.

--- src/modules/dmCommands/commands/steamOwner.js
+++ src/modules/dmCommands/commands/steamOwner.js
@@ -1,5 +1,5 @@
 import { MessageActionRow, MessageButton } from 'discord.js';
-import { getMember, getRole, userDb } from '../../../util';
+import { userDb } from '../../../util';
 
 export const command = 'steamowner';
 
@@ -10,15 +10,12 @@ export const command = 'steamowner';
  * @param {import('discord.js').GuildMember} member
  * @return {Promise<void>}
  */
-export async function fun(client, message, member) {
-    const role = getRole('steamowner');
+export async function fun(client, message) {
     const userDto = userDb.get(message.author.id);
     if (userDto.steamVia) {
         await message.author.send('You already either have the giveaway or steamowner role.');
         return;
     }
-    const filter = (i) => i.user.id === message.author.id;
-    const collector = message.channel.createMessageComponentCollector({ filter, time: 30000 });
     await message.author.send({
         content:
             'By becoming steam owner, you state, that you **bought** the game.\n' +
@@ -26,31 +23,16 @@ export async function fun(client, message, member) {
         components: [
             new MessageActionRow().addComponents(
                 new MessageButton({
-                    customId: 'steamownerAccept',
+                    customId: `steam.${message.author.id}.y`,
                     style: 'PRIMARY',
                     label: 'I want the role',
                 }),
                 new MessageButton({
-                    customId: 'steamownerDenied',
+                    customId: `steam.${message.author.id}.n`,
                     style: 'PRIMARY',
                     label: "Ok, i don't want it",
                 })
             ),
         ],
-    });
-    collector.on('collect', async (i) => {
-        console.log('Heeyyyyy click!!!!');
-        if (i.customId === 'steamownerAccept') {
-            await member.roles.add(role);
-            userDto.steamVia = 'steamowner';
-            userDb.set(message.author.id, userDto);
-            i.reply('You got the role now.');
-        }
-        collector.stop('successful');
-    });
-
-    collector.on('end', () => {
-        if (collector.total === 0) message.author.send('Time ran out');
-        else console.log('successful');
     });
 }
