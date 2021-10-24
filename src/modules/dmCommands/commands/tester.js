2c30062ad79cefa7420281905740e5196fafac95
Commentator2.0 <46420447+KommentatorForAll@users.noreply.github.com>
2021-10-24 22:33:19 +0200

    Update src/modules/dmCommands/commands/tester.js
    Co-authored-by: Antti <antti@antti.codes>

--- src/modules/dmCommands/commands/tester.js
+++ src/modules/dmCommands/commands/tester.js
@@ -11,7 +11,7 @@ export const desc = 'Testing the test, to test if testing the testing test, test
  * @return {Promise<void>}
  */
 export async function fun(client, message, member) {
-    const m = await member.fetch(true);
+    const m = await member.fetch();
     const role = getRole('tester');
     if (m.roles.cache.has(role)) {
         await m.roles.remove(role);
