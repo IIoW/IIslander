3fc0f0a6122e3470f76993be5d71045d9267220b
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2021-10-30 21:20:51 -0600

    feat: better auto mod
    Auto mod now ensures that swears are only detected if the word starts with the swear.
    Made regex swears use non-capturing groups (they are faster).
    Allows swears to have spaces between them.
    Pre-process the swear regex so that it is not processed wach time.
    
    This has been tested on all of our know false positives and no longer triggers.

--- src/modules/automod/message.js
+++ src/modules/automod/message.js
@@ -22,10 +22,11 @@ async function handleSwearing(client, message) {
         .replaceAll('5', 's')
         .replaceAll(/7|\+/g, 't')
         .replaceAll('8', 'b')
-        .replaceAll(/[^a-z]/g, '');
-    const regex = Blacklist.swearwords.join('|');
+        .replaceAll(/[^a-z ]/g, '');
+    const regex = Blacklist.swearRegex;
     const match = content.match(regex);
     if (match) {
+        const word = match[0].trim();
         const userDto = userDb.get(message.author.id);
         const now = Date.now();
         const cooldown = userDto.cooldown.get('swearing');
@@ -33,6 +34,7 @@ async function handleSwearing(client, message) {
             userDto.swearlevel = 0; // resets the swear level, if time ran out
         }
         userDto.swearlevel += 1;
+        message.delete().catch((e) => console.error('Error deleting swear', e));
         switch (userDto.swearlevel) {
             case 1:
                 await message.author
@@ -52,7 +54,7 @@ async function handleSwearing(client, message) {
                         .get('swearing')
                         .replace('[user]', message.member)
                         .replace('[message]', message.content)
-                        .replace('[match]', match[0])}`
+                        .replace('[match]', word)}`
                 );
         }
         userDto.cooldown.set('swearing', now + xpCooldown.swearing);
@@ -62,7 +64,7 @@ async function handleSwearing(client, message) {
                 .get('swearing')
                 .replace('[user]', message.member)
                 .replace('[message]', message.content)
-                .replace('[match]', match[0])
+                .replace('[match]', word)
         );
     }
 }
