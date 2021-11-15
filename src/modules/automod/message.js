c7887b98c9a9acf3ce9e5dd99598c159161e54c3
Commentator2.0 <46420447+KommentatorForAll@users.noreply.github.com>
2021-11-15 18:29:40 +0100

    folded if statement
    Co-authored-by: Wilson <33164598+WilsontheWolf@users.noreply.github.com>

--- src/modules/automod/message.js
+++ src/modules/automod/message.js
@@ -117,7 +117,7 @@ async function handlePings(client, message) {
 export async function messageCreate(client, message) {
     if (message.author.bot) return;
     // If the message is outside of the default guild we can ignore it
-    if (!message.guild || message.guild.id !== config.defaultGuild) return;
+    if (message.guild?.id !== config.defaultGuild) return;
     await handleSwearing(client, message);
     await handlePings(client, message);
 }
