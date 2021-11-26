fb66b162866c9aeeb838bf246eb4298bab1385fc
Wilson <33164598+WilsontheWolf@users.noreply.github.com>
2021-11-26 09:19:33 -0700

    chore: crash and burn
    Co-authored-by: Antti <antti@antti.codes>

--- src/modules/errorHandling/unhandledError.js
+++ src/modules/errorHandling/unhandledError.js
@@ -1,10 +1,12 @@
-import { getClient } from '../../util';
+import { getClient, userDb, responseDb, keyDb, factionDb } from '../../util';
 
 process.on('uncaughtException', (err) => {
     console.error('Uncaught Error!!!!\nSomebody better fix this!!!');
     console.error(err);
-    if (getClient()?.uptime < 1) {
-        console.log("Client doesn't seem to be up. Killing bot!!!");
-        process.exit(1);
-    }
+    getClient()?.destroy();
+    userDb.close();
+    responseDb.close();
+    keyDb.close();
+    factionDb.close();
+    process.exit(1);
 });
