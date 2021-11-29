893eff068f5c3565b17f0072b70ccbd574cff1ab
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2021-11-29 11:34:48 -0700

    fix: non-crystal cooldown notifications

--- src/modules/cooldownNotifications/module.js
+++ src/modules/cooldownNotifications/module.js
@@ -8,6 +8,8 @@ const subscriptions = new Map();
 subscriptions.set('cooldownEnd', async (client, reactionName, userid) => {
     const userDto = userDb.get(userid);
     if (!userDto.notifications.get('cooldownEnd')) return;
+    // Prevents non-crystal cooldowns from being sent.
+    if (!/tier\d\w+/.test(reactionName)) return;
     await (
         await client.users.fetch(userid)
     ).send(
