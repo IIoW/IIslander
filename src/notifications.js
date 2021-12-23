43195e7a5a96d2c0da9f455ab64a8bd92db79467
KommentatorForAll <46420447+KommentatorForAll@users.noreply.github.com>
2021-12-24 00:06:15 +0100

    fixed foramtting

--- src/notifications.js
+++ src/notifications.js
@@ -36,7 +36,7 @@ export default async function sendLevelNotification(member, level) {
 
     await member.fetch(false);
     return replaceAndSend(levelMessages[messageIndex], {
-        name: `**${level < 20? member.displayName: member}**`,
+        name: `**${level < 20 ? member.displayName : member}**`,
         level: `**Level ${level}**`,
     });
 }
