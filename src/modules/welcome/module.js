1715740d04b28688bd914506e54ff1154ba1e340
Antti <antti@antti.codes>
2022-03-28 21:25:10 +0300

    fix: sanitize username

--- src/modules/welcome/module.js
+++ src/modules/welcome/module.js
@@ -1,3 +1,4 @@
+import { Util } from 'discord.js';
 import config from '../../config';
 import { welcomeMessage, welcomeMessages, welcomeQuestions } from '../../constants/Messages';
 import ensureRoles from '../../roles';
@@ -20,7 +21,7 @@ subscriptions.set('guildMemberAdd', async (client, member) => {
     );
 
     await offtopicChannel.send(
-        `I have a question for you ${member.user.username}! ${
+        `I have a question for you ${Util.escapeMarkdown(member.user.username)}! ${
             welcomeQuestions[Math.floor(Math.random() * welcomeQuestions.length)]
         }`
     );
