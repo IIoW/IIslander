0e4882fd77ff35a0cd84373b38d89798a97f2a4f
Commentator2.0 <kommentator2.9.0.1@gmail.com>
2022-03-29 22:01:32 +0200

    removed db closes

--- src/modules/errorHandling/unhandledError.js
+++ src/modules/errorHandling/unhandledError.js
@@ -1,13 +1,8 @@
-import { factionDb, keyDb, responseDb, userDb } from '../../dbs';
 import { getClient } from '../../util';
 
 process.on('uncaughtException', (err) => {
     console.error('Uncaught Error!!!!\nSomebody better fix this!!!');
     console.error(err);
     getClient()?.destroy();
-    userDb.close();
-    responseDb.close();
-    keyDb.close();
-    factionDb.close();
     process.exit(1);
 });
